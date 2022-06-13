1e5c9eb8cb58b4d8b2eb711d24cde4c338f43af7
CedricBaude <baude.cedric.perso@gmail.com>
2022-06-13 19:58:11 +0200

    maj(2)

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 
-const num_cart = document.querySelector("#num_cart");
-const name_cart = document.querySelector("#name_cart");
+const num_card = document.querySelector("#num_card");
+const name_card = document.querySelector("#name_card");
 const expi_date= document.querySelector("#expi_date");
 const contenu = document.querySelector("#contenu");
 const form = document.querySelector("#form");
@@ -12,16 +12,25 @@ const form = document.querySelector("#form");
     console.log(event);
 }) */
 
-num_cart.addEventListener('input', (e) => {
+num_card.addEventListener('input', (e) => {
     
-    const value = num_cart.value;
-    contenu.textContent =` ${value}`;
-    
+    const value = num_card.value;
+    if (num_card.value.length === 0){
+    contenu.innerHTML ='<p>&bull;&bull;&bull;&bull;&bull;&bull;&bull;&bull;&bull;&bull;&bull;&bull;&bull;&bull;&bull;&bull;</p>';
+    }
+    if (num_card.value.length === 1 || num_card.value.length === 2 || num_card.value.length === 3 || num_card.value.length === 4 ||
+        num_card.value.length === 5 || num_card.value.length === 6 || num_card.value.length === 7 || num_card.value.length === 8 ||
+        num_card.value.length === 9 || num_card.value.length === 10 || num_card.value.length === 11 || num_card.value.length === 12 ||
+        num_card.value.length === 13 || num_card.value.length === 14 || num_card.value.length === 15 || num_card.value.length === 16 ||
+        num_card.value.length === 17 || num_card.value.length === 18 || num_card.value.length === 19 || num_card.value.length === 20 
+        ){
+        contenu.textContent =` ${value}`;
+        }
 });
 
-name_cart.addEventListener('input', (e) => {
+name_card.addEventListener('input', (e) => {
     
-    const value = name_cart.value;
+    const value = name_card.value;
     contenu2.textContent =` ${value}`;
     
 });
